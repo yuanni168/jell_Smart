8fb2e394361a0b8b775c98dd640f1b0b001ad1b6
jelly.luo <906176697@qq.com>
2018-04-18 16:57:24 +0800

    add file

---
+++ jellyCore/src/main/java/com/jia/jelly/jellycore/base/BaseFragment.java
@@ -0,0 +1,110 @@
+package com.jia.jelly.jellycore.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/5/8.
+ */
+
+public abstract class BaseFragment extends Fragment {
+
+    private static final String TAG = BaseFragment.class.getSimpleName();
+
+    private boolean isFragmentVisible;
+    private boolean isReuseView;
+    private boolean isFirstVisible;
+    private View rootView;
+
+
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (rootView == null) {
+            return;
+        }
+        if (isFirstVisible && isVisibleToUser) {
+            onFragmentFirstVisible();
+            isFirstVisible = false;
+        }
+        if (isVisibleToUser) {
+            onFragmentVisibleChange(true);
+            isFragmentVisible = true;
+            return;
+        }
+        if (isFragmentVisible) {
+            isFragmentVisible = false;
+            onFragmentVisibleChange(false);
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initVariable();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        if (rootView == null) {
+            rootView = view;
+            if (getUserVisibleHint()) {
+                if (isFirstVisible) {
+                    onFragmentFirstVisible();
+                    isFirstVisible = false;
+                }
+                onFragmentVisibleChange(true);
+                isFragmentVisible = true;
+            }
+        }
+        super.onViewCreated(isReuseView ? rootView : view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        initVariable();
+    }
+
+    private void initVariable() {
+        isFirstVisible = true;
+        isFragmentVisible = false;
+        rootView = null;
+        isReuseView = true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    /**
+     * @param isReuse
+     */
+    protected void reuseView(boolean isReuse) {
+        isReuseView = isReuse;
+    }
+
+    protected void onFragmentVisibleChange(boolean isVisible) {
+
+    }
+
+    protected void onFragmentFirstVisible() {
+
+    }
+
+    protected boolean isFragmentVisible() {
+        return isFragmentVisible;
+    }
+}
